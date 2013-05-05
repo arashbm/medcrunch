b2403f916920a30a88d93c7f7a9aa5fafacbf7eb
Arash Badie Modiri <arashbm@gmail.com>
2013-05-06 01:54:18 +0430

    removed debug print to decrease noise

--- app/workers/extractor.rb
+++ app/workers/extractor.rb
@@ -5,7 +5,6 @@ class ArticleExtractorWorker
     list = []
     Keyword.includes(:articles).find_each do |keyword|
       list.concat keyword.list_keyword_occurrence(ids)
-      puts keyword.title if keyword.id % 1000 == 0
     end
     ArticleKeyword.import [:keyword_id, :article_id], list, validate: false
   end
