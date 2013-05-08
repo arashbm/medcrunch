1ecdbce3a48daa40b6e13005c988021cf6de75ea
Arash Badie Modiri <arashbm@gmail.com>
2013-05-08 09:44:10 +0430

    removed unneeded `includes` call

--- app/workers/extractor.rb
+++ app/workers/extractor.rb
@@ -3,7 +3,7 @@ class ArticleExtractorWorker
 
   def perform(ids = nil)
     list = []
-    Keyword.includes(:articles).find_each do |keyword|
+    Keyword.find_each do |keyword|
       list.concat keyword.list_keyword_occurrence(ids)
     end
     ArticleKeyword.import [:keyword_id, :article_id], list, validate: false
