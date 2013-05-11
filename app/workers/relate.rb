5037f66ee56adfed0ac2fb3a998c8896ba2503cc
Arash Badie Modiri <arashbm@gmail.com>
2013-05-11 21:00:01 +0430

    better if

--- app/workers/relate.rb
+++ app/workers/relate.rb
@@ -3,11 +3,11 @@ class KeywordRelationWorker
 
   def perform(ids=nil)
     list = []
-    if ids && ids.is_a? Array
-      ks = Keyword.select(:id).where(id: ids).to_a
-    else
-      ks = Keyword.select(:id).to_a
-    end
+    ks = if ids
+           Keyword.select(:id).where(id: ids).to_a
+         else
+           Keyword.select(:id).to_a
+         end
     ks.each do |keyword|
       keyword.neighbour_keyword_ids.each do |k|
         list << [keyword.id, k['weight'].to_i, k['keyword_id'].to_i] if k['keyword_id'].to_i > keyword.id
